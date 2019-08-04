ca0fbcb8ec3669c176c163935e782a4167c568c6
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2019-08-04 22:52:27 +0530

    Add files via upload

---
+++ Function01.py
@@ -0,0 +1,16 @@
+def my_fun(fname):
+	print("My name is " +fname) 
+
+my_fun("Pavan")
+my_fun("Usha Pavan")
+my_fun("Poojari")
+
+print('_-------------------------------')
+
+def my_country(country ="India"):
+	print('My country is '+ country)
+
+my_country('Sweden')
+my_country('Japan')
+my_country('Canada')
+my_country()
