c52351e4b9b6467065010b69f48203401ecd252d
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2019-08-11 23:48:29 +0530

    Update Function.py

--- Function.py
+++ Function.py
@@ -6,3 +6,19 @@ mytripler = myfunc(3)
 
 print(mydoubler(11)) 
 print(mytripler(11))
+
+
+
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
