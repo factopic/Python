43442fdff2db79bf062ae23dbed5dabeaada2aec
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2019-09-17 21:42:22 +0530

    Largest String with out using Build in Function

---
+++ LargerStrWOBF.py
@@ -0,0 +1,19 @@
+'Python Program to Take in Two Strings and Display the Larger String without Using Built-in Functions
+
+string1=input("Enter first string:")
+string2=input("Enter second string:")
+count1=0
+count2=0
+
+for i in string1:
+      count1=count1+1	
+for j in string2:
+      count2=count2+1
+if(count1<count2):
+      print("Larger string is:")
+      print(string2)
+elif(count1==count2):
+      print("Both strings are equal.")
+else:
+      print("Larger string is:")
+      print(string1)
