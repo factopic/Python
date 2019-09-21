f99e94a51a57ebf6d01f655cb41f7a497fc22467
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2019-09-21 22:44:50 +0530

    Update List.py

--- List.py
+++ List.py
@@ -54,3 +54,20 @@ print(thistuple[2:5])
 
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(thistuple[-4:-1])
+
+
+
+
+
+n=int(input("Enter number:"))
+temp=n
+rev=0
+while(n>0):
+    dig=n%10
+    rev=rev*10+dig
+    n=n//10
+if(temp==rev):
+    print("The number is a palindrome!")
+else:
+    print("The number isn't a palindrome!")
+
