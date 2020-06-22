7e33377a3713790e28ae215dce17dda4cdaebf9c
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2020-06-22 22:36:23 +0530

    Update ReplaceSpaceWHyphen.py

--- ReplaceSpaceWHyphen.py
+++ ReplaceSpaceWHyphen.py
@@ -2,3 +2,11 @@ string=input("Enter string:")
 string=string.replace(' ','-')
 print("Modified string:")
 print(string)
+
+
+
+
+string=input("Enter string:")
+string=string.replace(' ','_')
+print("Modified string:")
+print(string)
