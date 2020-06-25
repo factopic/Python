82b53b18fab9a14eefe05e940eea1e0820f3cd88
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2020-06-25 22:07:05 +0530

    Update WordandChar.py

--- WordandChar.py
+++ WordandChar.py
@@ -23,3 +23,17 @@ print("Number of words in the string:")
 print(word)
 print("Number of characters in the string:")
 print(char)
+
+
+
+string=input("Enter string:")
+char=0
+word=1
+for i in string:
+      char=char+1
+      if(i==' '):
+            word=word+1
+print("Number of words in the string:")
+print(word)
+print("Number of characters in the string:")
+print(char)
