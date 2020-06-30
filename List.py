0f390d8b101bc652bce9650e5352958efd582c50
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2020-06-30 22:26:45 +0530

    Update List.py

--- List.py
+++ List.py
@@ -91,8 +91,4 @@ print("The length in feet",round(feet,2))
 
 
 
-cm=int(input("Enter the height in centimeters:"))
-inches=0.394*cm
-feet=0.0328*cm
-print("The length in inches",round(inches,2))
-print("The length in feet",round(feet,2))
+
